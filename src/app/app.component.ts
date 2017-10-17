daf93dac9eebce97ce7e095101b25c9a50d3e851
Will <w_priddle@hotmail.com>
2017-10-17 10:52:48 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name = 'Will';
+  elements: number[] = [];
+  message = '';
+
+  onChangeName() {
+  	this.name = 'Anna';
+  }
+
+  onAddElement() {
+  	this.elements.push(this.elements.length + 1);
+  }
+
+  getColor(element: number) {
+  	return element % 2 === 0 ? 'green': 'red';
+  }
+
+  onUserWasClicked(usrName: string) {
+  	alert(usrName);
+  }
 }
